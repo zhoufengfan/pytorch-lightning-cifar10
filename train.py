1e4e6e1f6aec03cd114eabdfc203c977f3def1af
zffustb <zhou_fengfan@163.com>
2021-07-16 22:19:35 +0800

    The warning is fixed

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 # The code is from `https://pytorch-lightning.readthedocs.io/en/latest/notebooks/starters/cifar10-baseline.html`. The copyright of this file belongs to the original authors of this file.
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.plugins import DDPPlugin
 
 from data_module import cifar10_dm
 from hyper_var import AVAIL_GPUS
@@ -9,13 +11,14 @@ from backbone import LitResnet
 if __name__ == '__main__':
     model = LitResnet(lr=2e-3)
     model.datamodule = cifar10_dm
-
+    checkpoint_callback = ModelCheckpoint(dirpath='lightning_logs/',monitor='val/loss')
     trainer = Trainer(
         progress_bar_refresh_rate=10,
         max_epochs=60,
         gpus=AVAIL_GPUS,
-        logger=TensorBoardLogger('lightning_logs/', name='resnet'),
-        accelerator='ddp'
+        callbacks=[checkpoint_callback],
+        accelerator='ddp',
+        plugins=DDPPlugin(find_unused_parameters=False),
     )
 
     trainer.fit(model, cifar10_dm)
